a547e4772d874c1443ebe04e126be46a56924d96
Daniil Yatsiny <maoirtx77@gmail.com>
2024-12-14 14:48:54 +0300

    T1: create base app

---
+++ text-serivce/src/main/java/com/example/text_serivce/TextSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.example.text_serivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextSerivceApplication.class, args);
+	}
+
+}
---
+++ text-serivce/src/test/java/com/example/text_serivce/TextSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.text_serivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
